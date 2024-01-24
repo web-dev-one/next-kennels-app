a4b3693aed9a9ee74911eba2272fc64b5171d3bc
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-01-23 21:16:28 -0700

    update

--- route53.tf
+++ route53.tf
@@ -31,6 +31,7 @@ resource "aws_acm_certificate" "hello_certificate" {
   domain_name       = var.domainName
 
   validation_method = "DNS"
+
   lifecycle {
     create_before_destroy = true
   }
@@ -42,7 +43,8 @@ resource "aws_route53_record" "hello_cert_dns" {
   records = [tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_value]
   type = tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_type
   zone_id = aws_route53_zone.kennelsdomain_name.zone_id
-  ttl = 60
+  ttl = 40
+ 
 }
 
 resource "aws_route53_record" "kennelsomain_name" {
