dd26126ebd7348d4e99610200184fec7a4355add
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-01-27 12:29:01 -0700

    update

--- ecs.tf
+++ ecs.tf
@@ -6,7 +6,7 @@ resource "aws_ecs_cluster" "main" {
 resource "aws_ecs_task_definition" "app" {
   family                   = "kennels-app-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  task_role_arn            = aws_iam_role.task_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.fargate_cpu
