330cd17b0db27bd42ad2d16092868b481aef83f9
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-01-27 09:12:13 -0700

    update

---
+++ ecsiam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role_policy_attachment" "execution_role_pass" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_passrole.arn
+}
+
+resource "aws_iam_policy" "ecs_execution_passrole" {
+  name        = "${var.name}-pass-to-ecs-exec"
+  path        = "/"
+  description = "Pass role to ecs service"
+  policy      = data.aws_iam_policy_document.execution_passrole.json
+}
+
+data "aws_iam_policy_document" "execution_passrole" {
+  statement {
+    actions = ["elasticloadbalancing:*", "application-autoscaling:*","resource-groups:*","iam:*", "sts:AssumeRole", "ecs:*", "cloudwatch:*", "codedeploy:*", "autoscaling:*"]
+    sid     = ""
+    effect  = "Allow"
+     resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
