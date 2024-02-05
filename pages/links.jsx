f5e25ee1036b7df14729127707dadda315f1da23
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-02-05 08:55:07 -0700

    update

--- pages/links.jsx
+++ pages/links.jsx
@@ -2,7 +2,8 @@ import Layout from "../components/Layout/Layout";
 import WelcomeCarousel from "../components/Carousel";
 
 import Links from "../components/Links";
-const links = [{title: "Pet-Safe-Kennels Info", href: "https://www.pet-safe-kennels.com", des: "for dogs"}, 
+const links = 
+[{title: "Pet-Safe-Kennels Info", href: "https://www.pet-safe-kennels.com", des: "for dogs"}, 
 {title: "Catios", href: "https://petsafecatios.com/"}, 
 {title: "Living with Coytoes", href: "https://www.azgfd.com/wildlife-conservation/living-with-wildlife/living-with-coyotes/#:~:text=Feed%20your%20pets%20inside%20and,sturdy%20enclosure%20with%20a%20roof."}, 
 {title: "East Valley k9 Services", href: "https://eastvalleyk9.com/how-to-protect-your-dog-from-coyotes-7-tips-and-more/"}, 
@@ -13,7 +14,10 @@ const links = [{title: "Pet-Safe-Kennels Info", href: "https://www.pet-safe-kenn
 {title: "Vet Loan Bill", href: "https://www.azhumane.org/news/sb-1271-passes-senate/"}, 
 {title: "Pet Insurance", href: "https://www.healthypawspetinsurance.com/locations/az/arizona-pet-insurance"}, 
 {title: "Dog Boarding", href: "https://www.dogtopia.com/phoenix-arcadia/"}, 
-{title: "Phoenix Dogs", href: "https://phxdogs.com/"}]
+{title: "Phoenix Dogs", href: "https://phxdogs.com/"},
+{title: "Goodyear Dog Care", href: "https://deercreekah.net/services/daycare/?cp=9289006312"},
+{title: "Tempe Doggie District", href: "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiZ3q-Yx5SEAxX-NtQBHXd0CAUYABAAGgJvYQ&ase=2&gclid=CjwKCAiAq4KuBhA6EiwArMAw1O68zr8oIQDAtskpYifkNbnAG_e0Vnr2n82_2DZ1YbnTsKxejrsxOBoCGPkQAvD_BwE&ohost=www.google.com&cid=CAESVuD2d16ib1xlRlMnWtYJfr9LviZw9sY1Y9JXBCcCyRrOSDHAe3Qc5VFbiV7I3FGsn9rHRbFz8d2zeZcua0OulH1-By_LXfFisO2ZcbYo_6jmr7nWXuuc&sig=AOD64_3qy0nmqegR-VnV7d7ss_PyHXERyg&q&nis=4&adurl&ved=2ahUKEwjJq6iYx5SEAxU3OUQIHYClAdQQ0Qx6BAgPEAM"},
+{title: "Scottsdale Paw Commons", href: "https://www.pawcommons.com/scottsdale-az/dog-boarding/?mm_campaign=scottsdale-boarding&gad_source=1&gclid=CjwKCAiAq4KuBhA6EiwArMAw1GwtoTZqqi4YIdiJC-T8nm7tiqoFnJg1D7gXFhjZbamr2ATRa9V72hoC6dUQAvD_BwE"}]
 
 export default function Waxing() {
   return (
