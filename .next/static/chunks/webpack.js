ec1c0237005cd3249c3b8bfc8183beb63cc6de38
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-02-06 17:35:22 -0700

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1f78b8754db07b1"; }
+/******/ 		__webpack_require__.h = function() { return "a54930f1b67c2e72"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/City.js
+++ pages/City.js
@@ -4,19 +4,19 @@ import Image from "next/image";
 export default function MyCity({id, name, img, des, detail}){
    
     return(<>
-            <div key={id} className="flex flex-col justify-center w-90 h-100 mx-auto mt-4">
+            <div key={id} className="flex flex-col justify-center w-100 h-100 mx-auto mt-4 px-4">
             <h1 className="fs-1 text-4xl text-red-500 text-center relative mt-14">{name}</h1>
             <h3 className="fs-1 text-xl text-sky-900 mx-auto">{des}</h3>
             <Image
              src={img}
-             width={450}
-             height={580}
+             width={550}
+             height={680}
              className="mx-auto justify-center"
              alt={`${name}`}
              quality={75}
              loading={"eager"}
             />
-            <h4 className="bg-white fs-1 text-xl text-sky-900 mx-auto">{detail}</h4>
+            <h4 className="bg-white fs-1 text-3xl text-sky-900 mx-auto">{detail}</h4>
             </div>
           </>)
 }
