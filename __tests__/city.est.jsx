12ccf888125c1ff07c0402110164054bdb1e010d
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-02-13 18:11:22 -0700

    update

--- __tests__/city.est.jsx
+++ __tests__/city.est.jsx
@@ -1,24 +1,24 @@
-// import './matchMedia.mock';
-// import City from"../pages/[city].js";
-// import"@testing-library/jest-dom";
-// import { useRouter } from 'next/router';
-// import mockRouter from 'next-router-mock';
-// import { fireEvent, render, screen, waitFor, act } from"@testing-library/react";
-// import WelcomeCarousel from "../components/Carousel.jsx";
+import './matchMedia.mock';
+import City from"../pages/[city].js";
+import"@testing-library/jest-dom";
+import { useRouter } from 'next/router';
+import mockRouter from 'next-router-mock';
+import { fireEvent, render, screen, waitFor, act } from"@testing-library/react";
+import WelcomeCarousel from "../components/Carousel.jsx";
 
-// jest.mock('next/router', () => jest.requireActual('next-router-mock'))
+jest.mock('next/router', () => jest.requireActual('next-router-mock'))
 
-// const CityExample = ({href = '/[city].js'}) =>{
-//     const router = useRouter();
-//     return(<City city={{id: 0, name:"New York", img:"/newyork.jpg", des:"New York Ney York", detail:"city that never sleeps"}}/>)
-// }
+const CityExample = ({href = '/[city].js'}) =>{
+    const router = useRouter();
+    return(<City city={{id: 0, name:"New York", img:"/newyork.jpg", des:"New York Ney York", detail:"city that never sleeps"}}/>)
+}
 
-// describe('City', () => {
-//   it('Renders a city on the City heading', () => {
-//     render(<CityExample  />)
+describe('City', () => {
+  it('Renders a city on the City heading', () => {
+    render(<CityExample  />)
  
-//     const city = screen.getByTestId('city')
+    const city = screen.getByTestId('city')
  
-//     expect(city).toBeInTheDocument()
-//   })
-// })
+    expect(city).toBeInTheDocument()
+  })
+})
